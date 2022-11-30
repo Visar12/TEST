a7ca761908a707dfa4837da554141b8ffc82c3e3
Visar12 <103035835+Visar12@users.noreply.github.com>
2022-11-30 08:26:48 +0100

    Add project files.

---
+++ TEST/Kat.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEST
+{
+    public class Kat
+    {
+        public string Farve = "grå";
+
+        public Kat(string farve)
+        {
+           Farve=farve;
+        }   
+        public void skiftfarve()
+        {
+            Farve="gul";
+            Console.WriteLine($"Din farve er {Farve}");
+        }
+    }
+}
---
+++ TEST/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using TEST;
+
+namespace TEST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kat kat1 = new("grå");
+            Console.WriteLine(kat1.Farve);
+
+            kat1.skiftfarve();
+        }
+    }
+}
